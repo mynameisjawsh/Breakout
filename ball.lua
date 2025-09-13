006b8d50a90cc99c94d3e290f73cc624f251bfc0
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-13 15:37:02 -0500

    ball now does stuff

--- ball.lua
+++ ball.lua
@@ -2,14 +2,60 @@ Ball = {}
 
 function Ball:load()
 
+  self.x = Screen.width / 2
+  self.y = Screen.height / 2
+  self.width = 20
+  self.height = 20
+  self.speed = 200
+  self.yVel = self.speed
+  self.xVel = 0
+
 end
 
 
 function Ball:update(dt)
 
+  self.y = self.y + self.yVel * dt 
+  self.x = self.x + self.xVel * dt
+
+
+  if checkCollision(self, Player) then
+
+    self.yVel = -self.yVel
+
+    local middleBall = self.x + self.width / 2
+    local middlePlayer = Player.x + Player.width / 2
+    local collisionPosition = middleBall - middlePlayer
+
+    self.xVel = collisionPosition * 6
+
+  end
+
+  if checkWalls(self, Screen) then
+
+    self.xVel = -self.xVel
+
+  end
+
+  if checkCeiling(self, Screen) then
+
+    self.yVel = -self.yVel
+
+  end
+
+  if checkFloor(self, Screen) then
+
+    self:load()
+
+  end
+
 end
 
 
 function Ball:draw()
+
+  love.graphics.setColor(1, 1, 1)
+
+  love.graphics.rectangle("fill", self.x, self.y, self.width,  self.height)
 
 end
--- collisions.lua
+++ collisions.lua
@@ -11,3 +11,48 @@ function checkWalls(a, b)
   end
 
 end
+
+
+function checkCeiling(a, b)
+
+  if a.y < b.y then
+
+    return true
+
+  else
+
+    return false
+
+  end
+
+end
+
+
+function checkFloor(a, b)
+
+  if a.y > b.y + b.height then
+
+    return true
+
+  else
+
+    return false
+
+  end
+
+end
+
+
+function checkCollision(a, b)
+
+  if a.x + a.width > b.x and a.x < b.x + b.width and a.y +    a.height > b.y and a.y < b.y + b.height then
+
+    return true
+
+  else
+
+    return false
+
+  end
+
+end
